70eeb0d4833bc68233387475fab486bf9a161168
sjtalkar <sjtalkar@live.com>
2020-12-12 16:52:10 -0800

    Committing the filter code implemented

---
+++ callbacks_spatial_filter.py
@@ -0,0 +1,179 @@
+import pandas as pd
+import numpy as np
+
+import folium
+from folium.features import DivIcon
+
+from datamanipulation_spatial_filter import *
+
+# Dataframes available to all functions
+rental_geo_df, rental_neighborhood_df, rental_grp_nbd_df = createSpatialData()
+
+
+def createRentalMap(df, fixed_radius, nbd_or_grp):
+    """
+    This function tkes in a dataframe and applies a fucntion that creates circle markers for every 
+        latitude and longitude in the dataframe
+
+    Args:
+        df ([dataframe]): [Dataframe containing latitude and longitude as well as column nbd_count_normalized
+                            for count of listings normalized ]
+        fixed_radius ([boolean]): [if true use 30 as fixed radius, if false use the count to determine size of circle]
+        nbd_or_grp ([String]): [neighbourhood/group]
+
+    Returns:
+        [Folium Map]: [Folium map]
+    """
+    # Initialize a Folium map. Center it to the mean of latitude and longitude of the entire dataset
+    this_map = folium.Map(
+        location=[df["latitude"].mean(), df["longitude"].mean()],
+        tiles="CartoDB positron",
+        zoom_start=13,
+    )
+
+    def plotDot(point_row, fixed_radius, nbd_or_grp):
+        """
+            This function adds elements to this_map. It adds CircleMarkers with fixed or varying radius
+            It also creates circles with tooltips
+            A text indicating the count is added close to the circle
+
+            Args : point_row - a row of data expected to contain following columns 
+                            a) latitude
+                            b) longitude
+                            c) neighbourhood_cleansed or neighbourhood_group_cleansed
+                            d) glow_marker_color
+
+            Returns : Folium map with circles added                    
+
+        """
+
+        if fixed_radius:
+            radius = 30
+        else:
+            radius = point_row["nbd_count_normalized"]
+
+        if nbd_or_grp == "neighbourhood":
+            text = f'{point_row["neighbourhood_cleansed"]}'
+        else:
+            text = f'{point_row["neighbourhood_group_cleansed"]}'
+
+        folium.CircleMarker(
+            location=[point_row["latitude"], point_row["longitude"]],
+            radius=radius,
+            weight=3,
+            color=point_row["glow_marker_color"],
+            fill_color=point_row["glow_marker_color"],
+            fill_opacity=0.7,
+        ).add_child(folium.Tooltip(f"{point_row.name}: {text}")).add_to(this_map)
+
+        # Add text of count within circle
+        folium.map.Marker(
+            [point_row["latitude"], point_row["longitude"]],
+            icon=DivIcon(
+                icon_size=(150, 36),
+                icon_anchor=(0, 0),
+                html='<div style="font-size: 9pt font-weight:bold">%s</div>' % text,
+            ),
+        ).add_to(this_map)
+        return
+
+    # Create circle markers for every listing by applying the plotDot function
+    if nbd_or_grp == "neighbourhood":
+        df[
+            [
+                "latitude",
+                "longitude",
+                "glow_marker_color",
+                "nbd_count_normalized",
+                "neighbourhood_cleansed",
+            ]
+        ].apply(plotDot, args=(fixed_radius, nbd_or_grp), axis=1)
+    else:
+        df[
+            [
+                "latitude",
+                "longitude",
+                "glow_marker_color",
+                "nbd_count_normalized",
+                "neighbourhood_group_cleansed",
+            ]
+        ].apply(plotDot, args=(fixed_radius, nbd_or_grp), axis=1)
+    return this_map
+
+
+def createListingSpatial(rental_geo_df):
+    this_map = folium.Map(
+        location=[rental_geo_df["latitude"].mean(), rental_geo_df["longitude"].mean()],
+        tiles="cartodbdark_matter",
+        zoom_start=13,
+    )
+
+    def plotDot(point_row):
+        is_available = "Yes" if point_row["has_availability"] == "t" else "No"
+
+        text = f'Neighborhood: {point_row["neighbourhood_cleansed"]}<br>Is available: {is_available}<br>Accommodates: {point_row["accommodates"]}<br>Rent: {point_row["price"]}'
+        folium.CircleMarker(
+            location=[point_row["latitude"], point_row["longitude"]],
+            radius=1,
+            weight=2,
+            color=point_row["glow_marker_color"],
+            fill_opacity=0.7,
+        ).add_child(folium.Tooltip(f"{text}")).add_to(this_map)
+        return
+
+    rental_geo_df[
+        [
+            "latitude",
+            "longitude",
+            "neighbourhood_cleansed",
+            "glow_marker_color",
+            "has_availability",
+            "accommodates",
+            "price",
+        ]
+    ].apply(plotDot, axis=1)
+    return this_map
+
+
+def saveNeighborhoodMapHTML(nbd):
+    """
+    Call this function to create the HTML of the neighborhood map
+    Input : Neighborhood group 
+    Returns : a filename of the folium map created and saved 
+    """
+    # TBD Add an all and then use All or nbd to filter the dataframe
+    if nbd == "All":
+        filtered_df = rental_neighborhood_df.copy()
+    else:
+        filtered_df = rental_neighborhood_df[
+            rental_neighborhood_df["neighbourhood_group_cleansed"] == nbd
+        ].copy()
+    this_map = createRentalMap(filtered_df, False, "neighbourhood")
+    filename = (
+        "NeighborhoodCountMap.html"
+        if nbd == "All"
+        else f"NeighborhoodCountMap-{nbd}.html"
+    )
+    this_map.save(filename)
+    return filename
+
+
+def saveListingMapHTML(nbd):
+    """
+        Call this function to create the HTML of the individual listings  map
+        Input : Neighborhood group 
+        Returns : a filename of the folium map created and saved 
+    """
+    # TBD Add an all and then use All or nbd to filter the dataframe (create a copy of it)
+    if nbd == "All":
+        filtered_df = rental_geo_df.copy()
+    else:
+        filtered_df = rental_geo_df[
+            rental_geo_df["neighbourhood_group_cleansed"] == nbd
+        ].copy()
+
+    this_map = createListingSpatial(filtered_df)
+
+    filename = "IndvListingsMap.html" if nbd == "All" else f"IndvListingsMap-{nbd}.html"
+    this_map.save(filename)
+    return filename
---
+++ datamanipulation_spatial_filter.py
@@ -0,0 +1,129 @@
+import pandas as pd
+import numpy as np
+
+import folium
+from folium.features import DivIcon
+
+from sklearn import preprocessing
+
+
+def standardizeDF(df, measure_name):
+    """
+        This function takes in a dataframe and creates and adds a column to it with the standardized value
+        Therefore, it makes mean = 0 and scales the data to unit variance.
+        Input: measure_name : column to be standardized
+        
+    """
+    name = f"{measure_name}_standardized"
+    scaler = preprocessing.StandardScaler()
+    df[name] = scaler.fit_transform(df[[measure_name]])
+    return df
+
+
+def normalizeDF(df, measure_name, min_range=0, max_range=1):
+    """
+        This function takes in a dataframe and creates and adds a column to it with the normalized value
+         MinMaxScaler scales all the data features in the range [0, 1] or else in the range [-1, 1] 
+         if there are negative values in the dataset.
+        Input: measure_name : column to be normalized
+    """
+    name = f"{measure_name}_normalized"
+    # Create the Scaler object
+    scaler = preprocessing.MinMaxScaler(feature_range=(min_range, max_range))
+    df[name] = scaler.fit_transform(df[[measure_name]])
+    return df
+
+
+# Read the data into a dataframe
+full_df = pd.read_csv("data\listings_1.csv")
+
+# These are the primary fields we are interested in
+def createSpatialData():
+    rental_geo_df = full_df[
+        [
+            "neighbourhood_cleansed",
+            "neighbourhood_group_cleansed",
+            "latitude",
+            "longitude",
+            "price",
+            "has_availability",
+            "accommodates",
+        ]
+    ].copy()
+
+    glow_color_list = [
+        "#befdb7",
+        "#1B03A3",
+        "#FEFCD7",
+        "#a60000",
+        "#FE019A",
+        "#4d4dff",
+        "#ff6ec7",
+        "#72c100",
+        "#ff073a",
+        "#e2ffdc",
+        "#c8362e",
+        "#f3cc03",
+        "#df00fe",
+        "#bb9a14",
+        "#98a2af",
+        "#a2a2a2",
+        "#ff3f03",
+    ]
+
+    glow_group_color = dict(
+        zip(
+            list(rental_geo_df.groupby("neighbourhood_group_cleansed").groups.keys()),
+            glow_color_list,
+        )
+    )
+    rental_geo_df["glow_marker_color"] = rental_geo_df[
+        "neighbourhood_group_cleansed"
+    ].map(glow_group_color)
+
+    # Create a new dataframe with average latitude an longitude per neighborhood (88 of these) and Max of the neighborhood group they belong in and max of the marker vcolor
+    rental_neighborhood_df = rental_geo_df.groupby(["neighbourhood_cleansed"]).agg(
+        {
+            "latitude": "mean",
+            "longitude": "mean",
+            "neighbourhood_group_cleansed": "max",
+            "glow_marker_color": "max",
+            "neighbourhood_cleansed": "count",
+        }
+    )
+
+    # Create the Scaler object
+    scaler = preprocessing.MinMaxScaler(feature_range=(0, 50))
+    rental_neighborhood_df["nbd_count_normalized"] = scaler.fit_transform(
+        rental_neighborhood_df[["neighbourhood_cleansed"]]
+    )
+
+    # Group the dataframe by neighbourhood groups
+    rental_grp_nbd_df = rental_geo_df.groupby(["neighbourhood_group_cleansed"]).agg(
+        {
+            "latitude": "mean",
+            "longitude": "mean",
+            "glow_marker_color": "max",
+            "neighbourhood_group_cleansed": "count",
+        }
+    )
+
+    # Create the Scaler object to scale the count of rentals value to between a certain range
+    scaler = preprocessing.MinMaxScaler(feature_range=(0, 60))
+    rental_grp_nbd_df["nbd_count_normalized"] = scaler.fit_transform(
+        rental_grp_nbd_df[["neighbourhood_group_cleansed"]]
+    )
+
+    return rental_geo_df, rental_neighborhood_df, rental_grp_nbd_df
+
+
+def get_nbdgroups():
+    """
+    This function returns a dictionary of Neighborhood groups to populate a selection filter
+    Input: None
+    Returns: Dictionary
+    """
+    nbd_groups = list(full_df.neighbourhood_group_cleansed.unique())
+    nbd_groups.sort()
+    return ["All"] + nbd_groups
+
